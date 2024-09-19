bee762850de5a047823cc5e3046b0bdfdb1dbe26
Mr. Mahesh rathod <maheshrathod2146@gmail.com>
2024-09-19 09:52:44 +0530

    Add files via upload

---
+++ singal_inheritances_class.py
@@ -0,0 +1,16 @@
+class Country:
+    def acceptCountry(self):
+      self.cname=input("enter the country name :")
+    def dispCountry(self):
+      print("Country name is :",self.cname)
+class State(Country):
+    def acceptState(self):
+       self.sname=input("enter the state name:")
+    def dispState(self):
+      print("State name is :",self.sname)
+
+obj=State()
+obj.acceptCountry()
+obj.acceptState()
+obj.dispCountry()
+obj.dispState()
